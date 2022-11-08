ae16b20e305d981afcaf471d49b410339664ee98
nesa91146@gmail.com <nesa91146@gmail.com>
2022-11-08 23:43:45 +0500

    Добавьте файлы проекта.

---
+++ Repository/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DbAccess.Models;
+
+namespace WebApp_2.Repository
+{
+    public interface IRepository
+    {
+        List<TeamModel> GetTeams();
+        List<Country> GetCountries();
+        List<TeamSponsor> GetSponsors();
+        Country GetCountryByID(int id);
+        TeamModel GetTeamByID(int id);
+        bool CreateNewTeam(TeamModel newTeam);
+        bool EditTeam(TeamModel editTeam);
+        bool DeleteTeam(TeamModel newTeam);
+
+        bool EditCountry(Country editCountry);
+        bool AddCountry(Country newCountry);
+
+    }
+}
---
+++ Repository/SqlRepository.cs
@@ -0,0 +1,89 @@
+﻿using DbAccess.Models;
+using System;
+using System.Collections.Generic;
+using DbAccess;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp_2.Repository
+{
+    public class SqlRepository : IRepository
+    {
+        private readonly AppDBContext _db;
+        public SqlRepository(AppDBContext db)
+        {
+            _db = db;
+        }
+        public bool CreateNewTeam(TeamModel newTeam)
+        {
+            if (newTeam is null) return false;
+
+            _db.Add(newTeam);
+            _db.SaveChanges();
+            return true;
+        }
+        public bool EditTeam(TeamModel editTeam)
+        {
+            if (editTeam is null) return false;
+
+            _db.Teams.Update(editTeam);
+            _db.SaveChanges();
+            return true;
+        }
+        public bool DeleteTeam(TeamModel team)
+        {
+            if (team is null) return false;
+
+            _db.Remove(team);
+            _db.SaveChanges();
+            return true;
+        }
+        public bool AddCountry(Country newCountry)
+        {
+            if (newCountry is null) return false;
+
+            _db.Add(newCountry);
+            _db.SaveChanges();
+            return true;
+        }
+
+        public bool EditCountry(Country editCountry)
+        {
+            if (editCountry is null) return false;
+
+            _db.Countries.Update(editCountry);
+            _db.SaveChanges();
+            return true;
+        }
+
+        public List<Country> GetCountries()
+        {
+            var countries = _db.Countries.ToList();
+            return countries;
+        }
+
+        public Country GetCountryByID(int id)
+        {
+            var country = _db.Countries.FirstOrDefault(x => x.ID == id);
+            return country;
+        }
+
+        public List<TeamSponsor> GetSponsors()
+        {
+            var sponsors = _db.TeamSponsors.ToList();
+            return sponsors;
+        }
+
+        public TeamModel GetTeamByID(int id)
+        {
+            var team = _db.Teams.FirstOrDefault(x => x.ID == id);
+            return team;
+        }
+
+        public List<TeamModel> GetTeams()
+        {
+            var teams = _db.Teams.ToList();
+            return teams;
+        }      
+    }
+}
